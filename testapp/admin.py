96b1feadbbf785a4b5ccf28194d2f6e81d63852a
Andrei Pashkevich <humqad@gmail.com>
2023-12-10 20:09:30 +0300

    Make test task

--- testapp/admin.py
+++ testapp/admin.py
@@ -1,1 +1,42 @@
 from django.contrib import admin
+
+from testapp.models import Contract
+from testapp.models import LoanApplication
+from testapp.models import Manufacturer
+from testapp.models import Product
+
+
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'manufacturers_id',
+        'date_created',
+    ]
+
+
+@admin.register(LoanApplication)
+class LoanApplicationAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'contract',
+        'date_created',
+    ]
+
+
+@admin.register(Manufacturer)
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'date_created',
+    ]
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'manufacturer',
+        'loan_application',
+        'date_created',
+    ]
--- testapp/models.py
+++ testapp/models.py
@@ -1,1 +1,103 @@
 from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Manufacturer name',
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Creation date',
+    )
+
+    class Meta:
+        verbose_name = 'Manufacturer'
+        verbose_name_plural = 'Manufacturers'
+
+    def __str__(self):
+        return self.name
+
+
+class Contract(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Contract title',
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Creation date',
+    )
+
+    class Meta:
+        verbose_name = 'Contract'
+        verbose_name_plural = 'Contracts'
+
+    def __str__(self):
+        return self.title
+
+    @property
+    def manufacturers_id(self):
+        return sorted(
+            LoanApplication.objects.filter(
+                contract__id=self.id,
+            )
+            .values_list('product__manufacturer', flat=True)
+            .distinct(),
+        )
+
+
+class LoanApplication(models.Model):
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Loan application title',
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Creation date',
+    )
+    contract = models.ForeignKey(
+        to=Contract,
+        on_delete=models.PROTECT,
+        related_name='loan_application',
+        verbose_name='Contract',
+    )
+
+    class Meta:
+        verbose_name = 'Loan application'
+        verbose_name_plural = 'Loan applications'
+
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=255,
+        verbose_name='Product name',
+    )
+    date_created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Creation date',
+    )
+    manufacturer = models.ForeignKey(
+        to=Manufacturer,
+        on_delete=models.PROTECT,
+        related_name='product',
+        verbose_name='Manufacturer',
+    )
+    loan_application = models.ForeignKey(
+        to=LoanApplication,
+        on_delete=models.SET_NULL,
+        related_name='product',
+        verbose_name='Loan application',
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
+    def __str__(self):
+        return self.name
